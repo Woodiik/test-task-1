f3752d73fa1ff414c5ecc2bb67bf8f4b37955dcf
Woodiik <vovikvova04@gmail.com>
2023-05-20 21:49:46 +0300

    fix bug

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-task-1">
+    <BrowserRouter basename="/test-taks-1">
       <App />
     </BrowserRouter>
   </React.StrictMode>
